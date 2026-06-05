08cd51b97ef6dd7b0c6e97b92df8babc4a5d1702
okrim898 <mirko.pazzi@edu.immaginazioneelavoro.it>
2026-06-05 17:42:58 +0200

    Delete pages directory

--- pages/script1.js
+++
@@ -1,9 +0,0 @@
-const ore = 999;
-
-function assenze() {
-    const n = parseInt(document.getElementById("progresso").value);
-    const oreAssenza = n * 6;
-    const orePresenti = ore - oreAssenza;
-    const percentuale = (100 * orePresenti) / ore;
-    document.getElementById("barra").style.width = percentuale + "%";
-}
